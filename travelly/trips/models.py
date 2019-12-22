250461dc4b8fb1700a110e82699a8003c2e53b8c
= <zainr7989@gmail.com>
2019-12-21 21:36:38 -0500

    [ADD] Models to trips app.

--- travelly/trips/models.py
+++ travelly/trips/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Trip(models.Model):
+    '''A journey taken by a User.'''
+    pass
+
+
+class Location(model.Model):
+    '''The place a User can depart from or arrive upon.'''
+    pass
