f5198947b6cec73539ebd998f3e9454de0bd7eb2
= <zainr7989@gmail.com>
2019-12-23 19:03:28 -0500

    [ADD] Import statements.

--- travelly/trips/views.py
+++ travelly/trips/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse, reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import (
+    CreateView,
+    UpdateView,
+    DeleteView)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.http import HttpResponseRedirect
+from trips.models import Trip
+from django.contrib.auth.models import User
+from notes.forms import NoteForm
 
 
 def show_trips(request):
