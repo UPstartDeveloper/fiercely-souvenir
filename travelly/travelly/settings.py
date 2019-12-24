14b91ed3679f31040987a7ba122087b70229c676
= <zainr7989@gmail.com>
2019-12-24 16:07:59 -0500

    [Update] Settings that involve whitenoise.

--- travelly/travelly/settings.py
+++ travelly/travelly/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'travelly.urls'
@@ -141,6 +142,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/'),
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # provision PostgreSQL for deployment
