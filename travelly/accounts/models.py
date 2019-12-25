f117a988cb37d875db0fdb7bc04723a4cb904c00
= <zainr7989@gmail.com>
2019-12-25 18:05:33 -0500

    [Add] Mugshot field to Profile model.

--- travelly/accounts/models.py
+++ travelly/accounts/models.py
@@ -8,7 +8,15 @@ from django.conf import settings
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL,
                                 on_delete=models.CASCADE)
+    mugshot = models.ImageField(upload_to='images/',
+                                default='images/user-icon.png',
+                                help_text="User profile image")
 
     def __str__(self):
         '''Return the related User's username.'''
         return f"{self.user.username}'s Profile"
+
+    def get_absolute_url(self):
+        '''Returns a fully qualified path for user profile.'''
+        path_components = {'pk': self.user.id}
+        return reverse('accounts:user_info', kwargs=path_components)
