bcaf9a5e314315adacba7c553e1937338c1bdf9b
= <zainr7989@gmail.com>
2019-12-25 14:21:08 -0500

    [Add] Sign up view controller, link to sign up page.

--- travelly/accounts/urls.py
+++ travelly/accounts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from accounts.views import launch_page
+from accounts.views import (
+    launch_page,
+    SignUpView,
+)
 
 app_name = 'accounts'
 urlpatterns = [
     path('', launch_page, name="launch"),
+    path('signup/', SignUpView.as_view(), name='new-user'),
 ]
--- travelly/accounts/views.py
+++ travelly/accounts/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
+from accounts.forms import SignUpForm
+from django.views.generic.edit import (
+    CreateView,
+    UpdateView,
+    DeleteView)
+from django.views.generic.detail import DetailView
+from accounts.models import Profile
+from django.urls import reverse, reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
 
 
 def launch_page(request):
     '''Return the landing page template.'''
     return render(request, 'accounts/home.html')
+
+
+class SignUpView(SuccessMessageMixin, CreateView):
+    '''Allows site visitors to set up a new account with Profile.'''
+    form_class = SignUpForm
+    success_url = reverse_lazy('accounts:login')  # not implemented yet
+    template_name = 'accounts/signup.html'
+    success_message = "Congratulations! You may now log in to Travelly."
+
+    def form_valid(self, form):
+        '''Save the new User, and set up their profile as well.'''
+        self.object = form.save()
+        profile = Profile.objects.create(user=self.object)
+        profile.save()
+        return super().form_valid(form)
