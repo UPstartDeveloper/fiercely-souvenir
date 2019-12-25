342b755676ede05addc7650db8feea83ba35aa8e
= <zainr7989@gmail.com>
2019-12-25 18:26:32 -0500

    [Update] Settings to use AWS S3 file storage.

--- travelly/travelly/settings.py
+++ travelly/travelly/settings.py
@@ -47,7 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'trips',
     'airlines',
-    'accounts'
+    'accounts',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -145,6 +146,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
      os.path.join(BASE_DIR, 'static'),
 ]
+
+# settings for using AWS S3 file storage
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 # provision PostgreSQL for deployment
