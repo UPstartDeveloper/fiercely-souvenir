965d59d631dbfb0efe63e0ed3a8c61a75ef809d3
= <zainr7989@gmail.com>
2019-12-25 13:53:41 -0500

    [Add] Profile model in accounts app.

--- travelly/accounts/models.py
+++ travelly/accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from travelly import settings
+from django.urls import reverse
+from django.conf import settings
 
-# Create your models here
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE)
+
+    def __str__(self):
+        '''Return the related User's username.'''
+        return f"{self.user.username}'s Profile"
