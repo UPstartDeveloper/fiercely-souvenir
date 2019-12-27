28bf7a745a61564c5127e5f0cfd8a99d65fe3855
= <zainr7989@gmail.com>
2019-12-27 13:06:13 -0500

    [Update] Types of models used in airlines.

--- travelly/airlines/models.py
+++ travelly/airlines/models.py
@@ -2,24 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Review(models.Model):
-    '''Comments that users have about their flying experience.'''
-    airline = models.ForeignKey('Airline', on_delete=models.PROTECT)
-    comments = ''  # charfield
-
-
-class Rating(models.Model):
-    '''A 1-5 star (5 is exceptional, 1 is terrible) rating of the airline.'''
-    airline = models.ForeignKey('Airline', on_delete=models.PROTECT)
-    stars = ''  # integer
-
-
-class Cost(models.Model):
-    '''The price paid to fly with an airline, by a user.'''
-    airline = models.ForeignKey('Airline', on_delete=models.PROTECT)
-    price = None  # decimal or flaotfield
-
-
 class Airline(models.Model):
-    '''Provides User with flying services.'''
-    passengers = models.ManyToManyField(User)
+    '''Represents a company that Users fly with.'''
+    pass
+
+
+class Review(models.Model):
+    '''Feedback a User has about their experience with a certain airline.'''
+    pass
