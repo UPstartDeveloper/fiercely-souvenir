765f486f36f03fe94c5e503de15dfac5cf86f8b4
= <zainr7989@gmail.com>
2019-12-27 11:24:14 -0500

    [Add] Model classes for airlines app.

--- travelly/airlines/models.py
+++ travelly/airlines/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 
 
+class Review(models.Model):
+    '''Comments that users have about their flying experience.'''
+    pass
+
+
+class Rating(models.Model):
+    '''A 1-5 star (5 is exceptional, 1 is terrible) rating of the airline.'''
+
+
+class Cost(models.Model):
+    '''The price paid to fly with an airline, by a user.'''
+    pass
+
+
 class Airline(models.Model):
     '''Provides User with flying services.'''
     pass
