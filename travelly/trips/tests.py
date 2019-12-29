6c5ee54af1f329ae218e11f55265369ad8559c36
= <zainr7989@gmail.com>
2019-12-29 18:24:53 -0500

    [Add] TestCases for views in trips app.

--- travelly/trips/tests.py
+++ travelly/trips/tests.py
@@ -5,6 +5,28 @@ class TripsTestCase(TestCase):
     def test_true_is_true(self):
         '''Canary test to make sure environment is working correctly.'''
         self.assertEqual(True, True)
+        """
         self.assertEqual(True, False, (
             'You are able to throw errors correctly.'
         ))
+        """
+
+
+class TripCreateTests(TestCase):
+    '''Tests for the TripCreate view.'''
+    pass
+
+
+class TripDetailTests(TestCase):
+    '''Tests for the TripDetail view.'''
+    pass
+
+
+class TripUpdateTests(TestCase):
+    '''Tests for the TripUpdate view.'''
+    pass
+
+
+class TripDeleteTests(TestCase):
+    '''Tests for the TripDelete view.'''
+    pass
