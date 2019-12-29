1d509e689c3fddbd294e4fdb99567fc23c61d03a
= <zainr7989@gmail.com>
2019-12-29 18:21:40 -0500

    [Add] Canary tests.

--- travelly/trips/tests.py
+++ travelly/trips/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TripsTestCase(TestCase):
+    def test_true_is_true(self):
+        '''Canary test to make sure environment is working correctly.'''
+        self.assertEqual(True, True)
+        self.assertEqual(True, False, (
+            'You are able to throw errors correctly.'
+        ))
