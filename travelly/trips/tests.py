6b3f778ac20e32a0df904299132c7db1973f5ba8
= <zainr7989@gmail.com>
2019-12-29 19:11:55 -0500

    [Fix] bootstrap.min.css manifest not found error.

--- travelly/trips/tests.py
+++ travelly/trips/tests.py
@@ -3,6 +3,7 @@ from django.test.client import RequestFactory
 from django.contrib.auth.models import User
 from trips.models import Trip
 from django.urls import reverse, reverse_lazy
+import trips.views as trips_views
 
 
 class TripsTestCase(TestCase):
@@ -14,6 +15,31 @@ class TripsTestCase(TestCase):
             'You are able to throw errors correctly.'
         ))
         """
+
+
+class TripListTests(TestCase):
+    '''Tests for the TripList view.'''
+    def setUp(self):
+        """Instantiate RequestFactory and User objects to pass POST requests
+           to the TripCreate view.
+
+        """
+        self.factory = RequestFactory()
+        self.user = User.objects.create(username='Abdullah',
+                                        email='abd@gmail.com',
+                                        password="Abdullah's passwd")
+        self.trip = Trip.objects.create(title="Summer Break",
+                                        passenger=self.user, arrive_at="BOS",
+                                        terminal='G')
+
+    def test_get_list_page(self):
+        '''A user is able to see all the Trips in the database on the list.'''
+        # add Trip objects to the database
+        self.trip.save()
+        # make sure that the trip is shown on the TripList view
+        get_request = self.factory.get('trips:all-trips')
+        response = trips_views.TripList.as_view()(get_request)
+        self.assertEqual(response.status_code, 200)
 
 
 class TripCreateTests(TestCase):
