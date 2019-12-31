a0f7971ff39826999521606e20649b59613ac240
= <zainr7989@gmail.com>
2019-12-31 12:41:15 -0500

    [Add] Airlines app TestCases.

--- travelly/airlines/tests.py
+++ travelly/airlines/tests.py
@@ -9,3 +9,29 @@ from airlines.views import (
     ReviewDelete
 )
 from airlines.models import Airline, Review
+from django.contrib.auth.models import User
+
+
+class AirlineAndReviewCreateTests(TestCase):
+    '''Users add Airline and Review objects to the database.'''
+    pass
+
+
+class AirlineListTests(TestCase):
+    '''The user sees all the Airlines that are in the database.'''
+    pass
+
+
+class AirlineDetailTests(TestCase):
+    '''User sees both details about the airline, and its reviews.'''
+    pass
+
+
+class AirlineAndReviewUpdateTests(TestCase):
+    '''Users who post about airlines or reviews arw able to edit their post.'''
+    pass
+
+
+class ReviewDeleteTests(TestCase):
+    '''User deletes their own review posts if they please.'''
+    pass
