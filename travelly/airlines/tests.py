6227d8acded39714f15c880c2caafd71f77c1210
= <zainr7989@gmail.com>
2019-12-31 13:14:30 -0500

    [Add] Test POSTing a new Airline.

--- travelly/airlines/tests.py
+++ travelly/airlines/tests.py
@@ -24,20 +24,22 @@ class AirlineAndReviewCreateTests(TestCase):
         self.user = User.objects.create(username='Abdullah',
                                         email='abd@gmail.com',
                                         password="Abdullah's passwd")
-        self.airline = Airline.objects.create(title='United Airlines',
+        self.airline = Airline.objects.create(title='Summer Break',
                                               verified=True)
         self.review = Review.objects.create(airline=self.airline,
                                             headline='Great Service!',
                                             rating=5,
                                             comments='Highly Recommended!',
                                             price=697.87)
+        self.airline_url = 'airlines:create_airline'
+        self.review_url = 'airlines:create_review'
 
     def test_get_create_airline_form(self):
         '''User requests to see the form to add a new airline.'''
         # user is already logged in, so they pass the LoginRequiredMixin
         self.assertTrue(self.user.is_authenticated)
         # user makes a GET request to AirlineCreate view
-        get_request = self.factory.get('airlines:create_airline')
+        get_request = self.factory.get(self.airline_url)
         get_request.user = self.user
         # user gets a valid response from the server
         response = AirlineCreate.as_view()(get_request)
@@ -49,7 +51,7 @@ class AirlineAndReviewCreateTests(TestCase):
         # user is already logged in, so they pass the LoginRequiredMixin
         self.assertTrue(self.user.is_authenticated)
         # user makes a GET request to AirlineCreate view
-        get_request = self.factory.get('airlines:create_review')
+        get_request = self.factory.get(self.review_url)
         get_request.user = self.user
         # user gets a valid response from the server
         response = ReviewCreate.as_view()(get_request)
@@ -58,11 +60,26 @@ class AirlineAndReviewCreateTests(TestCase):
 
     def test_insert_airline(self):
         '''User adds an airline to the database.'''
-        pass
+        # user is already logged in, so they pass the LoginRequiredMixin
+        self.assertTrue(self.user.is_authenticated)
+        # user fills out the form on the AirlineCreate page
+        title_for_airline = 'United Airlines'
+        form_data = {
+            'title': title_for_airline,
+        }
+        # user submits the form
+        post_request = self.factory.post(self.airline_url, form_data)
+        post_request.user = self.user
+        response = AirlineCreate.as_view()(post_request)
+        # the new airline object exists, and is NOT verified by default
+        new_airline = Airline.objects.get(title=title_for_airline)
+        self.assertTrue(new_airline, not None)
+        self.assertEqual(new_airline.verified, False)
 
     def test_insert_review(self):
         '''User adds a review for an airline.'''
-        pass
+        # user is already logged in, so they pass the LoginRequiredMixin
+        self.assertTrue(self.user.is_authenticated)
 
 
 class AirlineListTests(TestCase):
