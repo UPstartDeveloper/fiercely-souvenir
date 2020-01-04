3b8e5683ef0951d403e84f5adb43aeb549e7a29d
= <zainr7989@gmail.com>
2020-01-03 20:17:49 -0500

    [Add] API views.

--- travelly/api/views.py
+++ travelly/api/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from rest_framework.generics import (
+    ListCreateAPIView, RetrieveUpdateDestroyAPIView
+)
+from trips.models import Trip
+from airlines.models import Airline, Review
+from api.serializers import TripSerializer, AirlineSerializer, ReviewSerializer
 
-# Create your views here.
+
+class TripListOrCreate(ListCreateAPIView):
+    """Presents a list of all Trip instances.
+       Allows for the creation of single Trip.
+       Method handlers for GET and POST.
+
+    """
+    queryset = Trip.objects.all()
+    serializer_class = NoteSerializer
+
+
+class TripReadUpdateOrDelete(RetrieveUpdateDestroyAPIView):
+    """User is able to read, update, or delete a Trip instance.
+       Lookup uses the id field of the specific Trip object.
+        Work with GET, PUT, PATCH, or DELETE methods.
+
+    """
+    queryset = Trip.objects.all()
+    serializer_class = NoteSerializer
+
+
+class AirlineListOrCreate(ListCreateAPIView):
+    """Presents a list of all Airline instances.
+       Allows for the creation of single Airline.
+       Method handlers for GET and POST.
+
+    """
+    queryset = Airline.objects.all()
+    serializer_class = AirlineSerializer
+
+
+class AirlineReadUpdateOrDelete(RetrieveUpdateDestroyAPIView):
+    """User is able to read, update, or delete a Airline instance.
+       Lookup uses the id field of the specific Airline object.
+        Work with GET, PUT, PATCH, or DELETE methods.
+
+    """
+    queryset = Airline.objects.all()
+    serializer_class = AirlineSerializer
+
+
+class ReviewListOrCreate(ListCreateAPIView):
+    """Presents a list of all Review instances.
+       Allows for the creation of single Review.
+       Method handlers for GET and POST.
+    """
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+
+class ReviewReadUpdateOrDelete(RetrieveUpdateDestroyAPIView):
+    """User is able to read, update, or delete a Review instance.
+       Lookup uses the id field of the specific Review object.
+        Work with GET, PUT, PATCH, or DELETE methods.
+    """
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
