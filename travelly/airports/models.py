a639264a4204f9df9b7260193e86ee54fae76e19
= <zainr7989@gmail.com>
2020-01-08 19:11:20 -0800

    [Add] Models for airports app.

--- travelly/airports/models.py
+++ travelly/airports/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Address(models.Model):
+    '''The address of the airport the User is at.'''
+    pass
+
+
+class HotelSearch(models.Model, Address):
+    '''Represents a search a User makes for nearby hotels at an airport.'''
+    pass
+
+
+class CarRentalSearch(models.Model, Address):
+    '''Represents a search a User makes for nearby hotels at an airport.'''
+    pass
