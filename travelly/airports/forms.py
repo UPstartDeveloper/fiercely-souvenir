fd24d3e4d8e27d8accbbfa40c3b2525af5885cc1
= <zainr7989@gmail.com>
2020-01-08 19:39:25 -0800

    [Add] AirportForm class.

---
+++ travelly/airports/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from airports.models import AirportAddress
+
+
+class AirportForm(forms.ModelForm):
+    '''A form based on the AirportAddress model fields.'''
+    class Meta:
+        model = AirportAddress
+        fields = [
+            'title', 'address'
+        ]
