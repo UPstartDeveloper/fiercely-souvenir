0a02b926d3063eb3717f61ec6587678ed652985d
= <zainr7989@gmail.com>
2020-01-08 19:52:56 -0800

    [Add] URL pattern of AirportCreate.

--- travelly/airports/urls.py
+++ travelly/airports/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from airports.views import (
     AirportList,
+    AirportCreate,
 )
 
 app_name = 'airports'
 urlpatterns = [
     path('options/', AirportList.as_view(), name="home"),
+    path('add-airport/', AirportCreate.as_view(), name="add_airport"),
 ]
